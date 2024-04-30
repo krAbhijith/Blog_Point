42bd488d8754aca44c3be32743602e9ad35c09c0
Abhijith K R <abhijithkrofficial@gmail.com>
2024-04-30 19:02:09 +0530

    created accounts app and completed login and register views

---
+++ accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('register/', AccountRegisterView.as_view(), name='register-account'),
+    path('login/', AccountLoginView.as_view(), name='login-account'),
+]
---
+++ accounts/views.py
@@ -0,0 +1,37 @@
+from django.forms import BaseModelForm
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import CreateView
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
+class AccountRegisterView(CreateView):
+    model = User
+    fields = ['first_name', 'last_name', 'email']
+    template_name = 'accounts/index.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        password = self.request.POST.get('password')
+        form.instance.username = self.request.POST.get('email')
+        form.instance.password = make_password(password)
+
+        self.object = form.save()
+        login(self.request, self.object)
+        return HttpResponseRedirect(self.get_success_url())
+    
+
+class AccountLoginView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'accounts/index.html')
+
+    def post(self, request, *args, **kwargs):
+        username = self.request.POST.get('email')
+        password = self.request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
